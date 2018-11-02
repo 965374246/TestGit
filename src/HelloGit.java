c4ac127b86e9059709ffb0af35f85baba34c2e45
965374246 <965374246@qq.com>
2018-11-02 22:16:48 +0800

    first commit By HJL

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author HeJiaLe
+ * @description Git测试
+ * @create 2018-11-02 22:14
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git~lalala");
+    }
+}
