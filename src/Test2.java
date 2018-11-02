f43339b24380062e85c207adaebbdccc62f94e6a
965374246 <965374246@qq.com>
2018-11-02 22:26:25 +0800

    idea 版本提交测试2

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+/**
+ * @author HeJiaLe
+ * @description idea提交测试2
+ * @create 2018-11-02 22:24
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("提交测试2");
+    }
+}
